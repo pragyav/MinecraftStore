503c5ecf250b4b5a215eb855ffbb0a049988f44a
anshi <anshi>
2017-07-26 18:44:30 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Run App:
+
+- Install dependencies: `npm install`
+- Get your Instagram access token from http://instagram.pixelunion.net/
+- Add Instagram access_token to `server.js`
+- Run the server: `node server.js` or `nodemon server.js`
+- View at: http://localhost:8080
+
+App Demonstrates:
+
+- Processed a JS file with Node
+- Installed packages using npm
+- Created an HTTP server with Node and Express
+- Used a package to grab API data
+- Templated an application with EJS
+- Displayed data with EJS
+
+Endless Possibilities:
+
+The concepts here can apply to many different types of applications. Play around with the many
+packages available on npm. The possibilities of what can be done with Node are endless.
+One can pair it with a frontend framework like Angular and a database system like MongoDB to
+create a MEAN Stack application.
---
+++ server.js
@@ -0,0 +1,37 @@
+// GRAB THE PACKAGES/VARIABLES WE NEED
+// ==================================================
+var express = require('express');
+var app     = express();
+var ig      = require('instagram-node').instagram();
+
+// CONFIGURE THE APP
+// ==================================================
+// tell node where to look for site resources
+app.use(express.static(__dirname + '/public'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// configure instagram app with access_token
+ig.use({
+  // get access token here: http://instagram.pixelunion.net/
+  access_token: 'ACCESS_TOKEN',
+});
+
+// SET THE ROUTES
+// ===================================================
+// home page route - popular images
+app.get('/', function(req, res) {
+
+    // use the instagram package to get popular media
+    ig.user_self_media_recent(function(err, medias, pagination, remaining, limit) {
+        // render the home page and pass in the popular images
+        res.render('pages/index', { grams: medias });
+    });
+
+});
+
+// START THE SERVER
+// ==================================================
+app.listen(8080);
+console.log('App started! Look at http://localhost:8080');
